0e17cecdadbb30ddd8f9f18ed0d6f6019ab7ae29
A-Novychenko <A.Novychenko@outlook.com>
2025-03-06 00:57:32 +0200

    add js var const let

--- js/js/index.js
+++ js/js/index.js
@@ -1,13 +1,75 @@
 // "use strict"
 
-console.log('firstVarBefore', firstVar)
+// console.log('firstVarBefore', firstVar)
 
-console.log('x1', x)
+// console.log('x1', x)
 
-x = 5
+// x = 5
 
-console.log('x', x)
+// console.log('x', x)
 
-var firstVar
+// var firstVar
 
-console.log('firstVarAfter', firstVar)
+// console.log('firstVarAfter', firstVar)
+
+// var x = y,
+
+
+// y = 'A'
+
+// // console.log('x', x)
+// console.log('y', y)
+
+// // var x = y,y = "A";
+// // console.log(x + y); // undefinedA
+
+
+// var x = 0;
+
+// function f() {
+//     var x = (y = 1); // x - объявляется локально. y - глобально!
+//     console.log('xf', x)
+// }
+// f();
+
+// console.log("x:",x, "y:",y); // 0, 1
+// // значение x взято из глобальной переменной, как и ожидалось
+// // значение переменной y доступно глобально
+
+
+// const a = 1
+// let b = 2
+// var c = 3
+// d = 4
+
+// function foo () { 
+//     var eee = 5
+//     rrr = 10
+//     const ccc = 5
+
+//     return function () { 
+//         console.log('eee', eee)
+//         console.log('rrr', rrr)
+//         console.log('ccc', ccc)
+//     }
+// }
+
+
+// const closure = foo()
+
+// console.dir(closure)
+
+// console.log('window', window)
+
+// console.log('window1', window)
+
+// const foo1 = function () { 
+//     console.log('foo1' )
+// }
+
+// function foo2 () { 
+//     console.log('foo2' )
+// }
+
+// console.log('window', window)
+
